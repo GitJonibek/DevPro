a3e9a0f714ec8920f798d9df6ea1fb4d4e0ab5fa
GitJonibek <jonibeknorboev@gmail.com>
2020-04-19 01:24:09 +0900

    Prepared for deploymentheroku git:remote -a aqueous-eyrie-81271

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 // connect database
 connectDB();
@@ -14,6 +15,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+// Serve static assets in production
+if(process.env.NODE_ENV) {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 // Listen
