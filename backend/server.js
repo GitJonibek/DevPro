30b062727950d99a3661be02b3091f06d9ec37f9
GitJonibek <jonibeknorboev@gmail.com>
2020-04-19 01:49:57 +0900

    Prepared for deploymentheroku git:remote -a aqueous-eyrie-81271

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV) {
+if(process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
