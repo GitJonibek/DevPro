9c9cd9dedbed2d83650807224ed35a4ac62e1fe2
GitJonibek <jonibeknorboev@gmail.com>
2020-05-16 08:20:42 +0900

    Change URL

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,6 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 // Listen
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
